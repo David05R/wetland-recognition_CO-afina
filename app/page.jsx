a859419a0b455393a2d928162ca5107f8b7226f5
daniherreraa <herreraacevedodl@gmail.com>
2024-07-21 11:58:50 -0500

    feat: Actualizar diseño de la página de inicio y agregar componentes de Next.js

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="relative w-screen min-h-screen justify-centerflex flex-col bg-background-dark overflow-hidden">
-      <div className="absolute flex flex-col items-start top-10 left-10 z-10">
+      <div className="absolute flex flex-col items-start top-14 left-14 z-10">
         <h1 className="font-poppins font-semibold text-white text-5xl max-w-[60svw]">
           Te damos la bienvenida al prototipo de Wetlands.io
         </h1>
         <Link href="/map">
-          <div className="bg-white text-primary mt-6 px-6 w-fit h-11 flex items-center rounded-full font-poppins">
+          <div className="bg-white text-primary mt-4 px-6 w-fit h-11 flex items-center rounded-full font-poppins">
             Inicia Wetlands.io
           </div>
         </Link>
